93a728520772b959c200f2b0057408ce7a535323
filipesantos.lf <filipesantos.lf@gmail.com>
2022-06-26 19:28:03 -0300

    [update] Graphql Codegen: adicionado o codegen do graphql

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:'https://api-sa-east-1.graphcms.com/v2/cl4ouifph11sm01yw86w91uoi/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` 
+    },
     cache: new InMemoryCache()
 
 })
